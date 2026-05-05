a257ccd86ef7f6624d8d73f3dc5ea2c5ef18145c
Alex Caras <alexcaras1@hotmail.com>
2026-05-05 17:53:37 -0300

    refactor: remove DOMPurify dependency and sanitize content directly in PostBody component

--- components/blog/PostBody.tsx
+++ components/blog/PostBody.tsx
@@ -1,16 +1,12 @@
-import DOMPurify from "isomorphic-dompurify";
-
 interface PostBodyProps {
   content: string;
 }
 
 export function PostBody({ content }: PostBodyProps) {
-  const sanitizedContent = DOMPurify.sanitize(content);
-
   return (
     <div
       className="prose prose-cocriart max-w-none"
-      dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+      dangerouslySetInnerHTML={{ __html: content }}
     />
   );
 }
